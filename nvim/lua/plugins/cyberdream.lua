1e8ccbfd91c82f0d95ed084ef2db7a1016e93346
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-09-26 23:27:40 -0600

    Updating Config with alpha header

--- nvim/lua/plugins/cyberdream.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "scottmckendry/cyberdream.nvim",
-    name = "cyberdream",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "cyberdream"
-    end
-}
